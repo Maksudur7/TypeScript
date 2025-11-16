a52f63ed38e0ac115483b56726639c24b25c08f8
Maksudr <maksudurrahamanmishu7@gmail.com>
2025-11-17 00:02:17 +0600

    compleated module 7

--- src/getterAndSetter.ts
+++ src/getterAndSetter.ts
@@ -1,30 +1,48 @@
 // access >> modify
 
 class BankAccount {
-  public readonly userId: number;
-  public userName: string;
-  private _userBalance: number;
+    public readonly userId: number;
+    public userName: string;
+    private _userBalance: number;
 
-  constructor(userId: number, userName: string, userBalance: number) {
-    this.userId = userId;
-    this.userName = userName;
-    this._userBalance = userBalance;
-  }
+    constructor(userId: number, userName: string, userBalance: number) {
+        this.userId = userId;
+        this.userName = userName;
+        this._userBalance = userBalance;
+    }
 
-  private addBalance(balance: number) {
-    this._userBalance = this._userBalance + balance;
-  }
+    // balance k set kortase
+    //    addBalance(balance: number) {
+    //     this._userBalance = this._userBalance + balance;
+    //   }
+
+    // seter use kora korta chai
+
+    set addBalance(amount: number) {
+        this._userBalance = this._userBalance + amount
+    }
+
+    // get korbo
+
+    // getBalance() {
+    //     return this._userBalance;
+    // }
+    // getter use kora get korta chai
+
+    get getBalance(){
+       return this._userBalance
+    }
 }
 
-class StudentBankAccount extends BankAccount {
-  test() {
-    this.userBalance;
-  }
-}
+
 
 const mezbaBhaiAccount = new BankAccount(111, "Mezba", 20);
 
-mezbaBhaiAccount.addBalance(100);
-mezbaBhaiAccount.addBalance(50);
+// mezbaBhaiAccount.addBalance(100);
+// mezbaBhaiAccount.addBalance(50);
 
+// console.log(mezbaBhaiAccount.getBalance());
+
+mezbaBhaiAccount.addBalance = 100;
+mezbaBhaiAccount.addBalance = 60;
 console.log(mezbaBhaiAccount);
